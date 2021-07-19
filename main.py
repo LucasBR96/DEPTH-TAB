ebc8d0eef44f8547e4dc3d236582b8c042828515
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2021-07-19 18:35:14 -0300

    preciso aprender regex e argv

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import src
 import sys
+import os
 
 def build_from_file( filename ):
     pass
 
 def build_from_cmd( ):
-    pass
+    
+    m = -1
+    while( m not in set( [ 0 , 1 ] ) ):
+        print( "type 1 for directed graf, 0 for undirected:" )
+        try:
+            m = int( input() )
+        except:
+            continue
 
 def main( arglst ):
-    pass
+    
+    option == arglst[ 0 ]
+    if option == "-c": # read graph from commmand line ------------------------------------------
+        build_from_cmd()
+    elif option == "-f": # read graph from file -------------------------------------------------
+        filename = arglst[ 1 ]
+        build_from_file( filename )
+
 
 if __name__ == "__main__":
-
-    arglst = ( sys.argv ).split()
-    main( arglst )
+    main( sys.argv )
