f4aae7e25e1438994a52e5d60919bbbbf01b731c
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2021-07-21 18:57:44 -0300

    comentarios do src.py feitos, falta revisar

--- src.py
+++ src.py
@@ -135,7 +135,7 @@ def depth_tab( V , E , direc ):
 def print_depth_tab( dtab ):
 
     '''
-    representation of a depth tab, rows are sorted by the colomn START in ascending
+    representation of a depth tab, rows are sorted by the column START in ascending
     order
     '''
 
@@ -149,14 +149,79 @@ def print_depth_tab( dtab ):
 
 def build_dfstimeline( dep_tab ):
 
+    '''
+    This function generates a 'Timeline' of a depth first search. Such timeline is 
+    a multi line string representing that visualy displays the succession of events
+    of depth first search executed over a given graph.
+
+    Each row of this table is a depth level of the search, while each column is a 
+    click from the depth_tab() function. The interval where a node is explored is
+    tepresented by a string that begins and ends with the node's name and is placed
+    in the row corresponding with its depth.
+
+    For example, the directed graph with edges:
+        
+        ( 0 , 1 )
+        ( 0 , 2 )
+        ( 0 , 4 )
+        ( 1 , 5 )
+        ( 2 , 1 )
+        ( 2 , 3 )
+        ( 3 , 4 )
+        ( 3 , 7 )
+        ( 4 , 1 )
+        ( 4 , 2 )
+        ( 4 , 7 )
+        ( 5 , 7 )
+        ( 6 , 0 )
+        ( 6 , 2 )
+        ( 6 , 5 )
+
+    Have the following adjency table
+
+        0 | 1 2 4
+        1 | 5
+        2 | 1 3
+        3 | 4 7
+        4 | 1 2 7
+        5 | 7
+        6 | 0 2 5
+        7 |
+
+    That outputs the depth table( starting the search at zero and searching in ascending order):
+
+        0 | 0 13 0
+        1 | 1 6 1
+        2 | 7 12 1
+        3 | 8 11 2
+        4 | 9 10 3
+        5 | 2 5 2
+        6 | 14 15 0
+        7 | 3 4 3
+
+    Based on this information, the seach will have as timeline:
+ 
+        0-----------0 6 
+         1---1 2---2    
+          5-5   3-3     
+           7     4    
+
+    Note: ( timeline doesn't looks good, finish later )
+    '''
+    
+    # -----------------------------------------------------------------------------
+    # if counting starts with zero, then its resonable the number of
+    # columns to be equal to the maximum end plus 1
     f1 = lambda x: dep_tab[ x ][ END ] + 1
     width = max( map( f1 , dep_tab.keys() ) )
-
-    f2 = lambda x: dep_tab[ x ][ DEPTH ]
+    # -----------------------------------------------------------------------------
+     
+    # same goes with depth --------------------------------------------------------
+    f2 = lambda x: dep_tab[ x ][ DEPTH ] + 1
     depth  = max( map( f2 , dep_tab.keys() ) )
     
     seq =[ " " ]*width
-    mat = [ seq.copy() for x in range( depth + 1 ) ]
+    mat = [ seq.copy() for x in range( depth ) ]
     # print( *mat, sep = "\n" )
     for node , ( s , e , d ) in dep_tab.items( ):
         nstr = str( node )
@@ -169,7 +234,7 @@ def build_dfstimeline( dep_tab ):
                 continue
             mat[ d ][ i ] = "-"
 
-    s = "\n".join( [ "".join( mat[ i ][:] ) for i in range( depth + 1 ) ] )
+    s = "\n".join( [ "".join( mat[ i ][:] ) for i in range( depth  ) ] )
     return s
 
 def dfs_results( V , E , direc  ): 
@@ -231,4 +296,3 @@ def test_1( ):
 if __name__ == "__main__":
 
    test_1()
-
